87c5cf1d658e043acafcc278002a7790fa6ffec4
abdulrahman hussien <abdulrahman2072022091@gmail.com>
2025-02-28 03:13:43 +0200

    ADD templates (about, contact, parent_page)& static files and Update app.py & index.html

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def landing_page():
-    return render_template('index.html',name="index page")
+    return render_template('index.html',name="index page", style='home.css')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html', name='About page', style='about.css')
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    return render_template('contact.html',name="contact page")
 
 if __name__ == '__main__':
     app.run(debug=True)
