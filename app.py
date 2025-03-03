0073343acfa66f793de13f3705beb506d045bf68
abdulrahman hussien <abdulrahman2072022091@gmail.com>
2025-03-03 17:33:08 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def landing_page():
     return render_template('index.html',name="index page", style='home.css')
 
